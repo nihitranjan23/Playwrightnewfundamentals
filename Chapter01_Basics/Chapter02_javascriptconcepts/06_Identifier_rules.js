f099439702af1e99e2f1288ee8fe5f9d92cab93c
Nihit Ranjan <ranjan.nihit@gmail.com>
2026-05-18 14:16:32 +0530

    Add Chapter 2 JavaScript concepts and hotcode file

---
+++ Chapter01_Basics/Chapter02_javascriptconcepts/06_Identifier_rules.js
@@ -0,0 +1,21 @@
+var as$ = 10;
+var _a = 12;
+var p = 10;
+
+var ab123 = 34;
+
+// var 123 = 123;
+
+var Name = "pramod";
+var name = "Amit";
+//var pramod dutta = "hello";
+var pramod_dutta = "hello";
+var pramod$dutta = "hello";
+var pramodu1232 = "hello";
+
+//console.log(as$);
+//console.log(_a);
+//console.log(ab123);
+//console.log(123);
+console.log(Name);
+console.log(name);
---
+++ Chapter01_Basics/Chapter02_javascriptconcepts/08_comments.js
@@ -0,0 +1,21 @@
+// This is single comment this will be ignore 
+// this line will be not executed
+
+
+/*
+  This is multi line
+ *  Author : Prrmmod Dutta
+ *  Date : 14-Feb-2026
+ */
+
+/**
+ *  This is multi line
+ *  Author : Prrmmod Dutta
+ *  Date : 14-Feb-2026
+ **/
+
+/*(var la=34;
+var j=98;
+console.log(la);
+console.log(j);
+*/
